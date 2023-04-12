4363b950db65669321220f069448bd884309a3a9
Terry Lamb <terrylamb@gmail.com>
2023-04-12 19:05:51 -0400

    Remove bad Booleans

--- arrays.rb
+++ arrays.rb
@@ -1,10 +1,12 @@
 students = ["student1" , "student2" , "student3" , "student4"]
+students.pop
+# Pop will remove the last elemnt of the array, in this case "student4"
+
+positions = [1 , 2 , 3 , 4]
+positions.push(5 , 6 , 7)
+# Push will 5,6 and 7 to the end of the array.
+
+temps = [98.6 , 105.2 , 100.1 , 110.8]
 
 
-position = [1 , 2 , 3 , 4]
-
-
-temp = [98.6 , 105.2 , 100.1 , 110.8]
-
-
-numbers = [paid = true, paid = false , late = true , late = false]
+numbers = [true, false, true, false]
