ed3fd1adc9108e39154197e70e0cd61a4e72f352
Terry Lamb <terrylamb@gmail.com>
2023-04-12 18:57:46 -0400

    Add 4 different arrays

---
+++ arrays.rb
@@ -0,0 +1,10 @@
+students = ["student1" , "student2" , "student3" , "student4"]
+
+
+position = [1 , 2 , 3 , 4]
+
+
+temp = [98.6 , 105.2 , 100.1 , 110.8]
+
+
+numbers = [paid = true, paid = false , late = true , late = false]
